2f824791224a654ab06f7ca3cdf9d06e461a44d2
Mariano Majano Amaya <mariano@bu.edu>
2024-05-13 16:41:33 -0400

    Working on details when user clicks on a shoe

---
+++ app/src/main/java/com/example/solemate/views/DetailedActivity.java
@@ -0,0 +1,58 @@
+package com.example.solemate.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.solemate.R;
+import com.example.solemate.utils.model.ShoeCart;
+import com.example.solemate.utils.model.ShoeItem;
+import com.example.solemate.viewmodel.CartViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    private ImageView shoeImageView;
+    private TextView shoeNameTV, shoeBrandNameTV, shoePriceTV;
+    private AppCompatButton addToCartBtn;
+    private ShoeItem shoe;
+    private CartViewModel viewModel;
+    private List<ShoeCart> shoeCartList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+
+        shoe = getIntent().getParcelableExtra("shoeItem");
+        initializeVariables();
+
+        viewModel.getAllCartItems().observe(this, new Observer<List<ShoeCart>>() {
+            @Override
+            public void onChanged(List<ShoeCart> shoeCarts) {
+                shoeCartList.addAll(shoeCarts);
+            }
+        });
+
+        if (shoe != null) {
+            setDataToWidgets();
+        }
+
+        addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertToRoom();
+            }
+        });
+
+    }
+}
