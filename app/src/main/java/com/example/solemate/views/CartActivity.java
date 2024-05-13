23ada733a8691643ada9758051febde7b5743f5a
Mariano Majano Amaya <mariano@bu.edu>
2024-05-13 16:33:09 -0400

    Adding drawable files

---
+++ app/src/main/java/com/example/solemate/views/CartActivity.java
@@ -0,0 +1,22 @@
+package com.example.solemate.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.solemate.R;
+import com.example.solemate.utils.adapter.CartAdapter;
+import com.example.solemate.utils.model.ShoeCart;
+import com.example.solemate.viewmodel.CartViewModel;
+
+import java.util.List;
+public class CartActivity {
+}
