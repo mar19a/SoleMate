de9dc7556dd553ccf67f214fdeff5e8124ff2c0e
Mariano Majano Amaya <mariano@bu.edu>
2024-05-13 01:03:31 -0400

    Working on Room db

---
+++ app/src/main/java/com/example/solemate/database/CartDatabase.java
@@ -0,0 +1,26 @@
+package com.example.solemate.database;
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.solemate.dao.CartDAO;
+import com.example.solemate.utils.model.ShoeCart;
+
+@Database(entities = {ShoeCart.class} , version = 1)
+public abstract class CartDatabase extends RoomDatabase {
+
+    public abstract CartDAO cartDAO();
+    private static CartDatabase instance;
+
+    public static synchronized  CartDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext()
+                            , CartDatabase.class , "ShoeDatabase")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
