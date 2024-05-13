4195a10914f5e22af0711886d242dada27f1827f
Mariano Majano Amaya <mariano@bu.edu>
2024-05-13 16:08:52 -0400

    Binding View Shoe Item

--- app/src/main/java/com/example/solemate/utils/adapter/ShoeItemAdapter.java
+++ app/src/main/java/com/example/solemate/utils/adapter/ShoeItemAdapter.java
@@ -16,4 +16,42 @@ import java.util.List;
 
 public class ShoeItemAdapter extends RecyclerView.Adapter<ShoeItemAdapter.ShoeItemViewHolder> {
 
+    private List<ShoeItem> shoeItemList;
+    private ShoeClickedListeners shoeClickedListeners;
+    public ShoeItemAdapter(ShoeClickedListeners shoeClickedListeners){
+        this.shoeClickedListeners = shoeClickedListeners;
+    }
+    public void setShoeItemList(List<ShoeItem> shoeItemList){
+        this.shoeItemList = shoeItemList;
+    }
+    @NonNull
+    @Override
+    public ShoeItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.each_shoe , parent , false);
+        return new ShoeItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ShoeItemViewHolder holder, int position) {
+        ShoeItem shoeItem = shoeItemList.get(position);
+        holder.shoeNameTv.setText(shoeItem.getShoeName());
+        holder.shoeBrandNameTv.setText(shoeItem.getShoeBrandName());
+        holder.shoePriceTv.setText(String.valueOf(shoeItem.getShoePrice()));
+        holder.shoeImageView.setImageResource(shoeItem.getShoeImage());
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shoeClickedListeners.onCardClicked(shoeItem);
+            }
+        });
+
+        holder.addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shoeClickedListeners.onAddToCartBtnClicked(shoeItem);
+            }
+        });
+    }
+
 }
