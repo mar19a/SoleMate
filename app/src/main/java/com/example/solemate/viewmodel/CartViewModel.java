f5487fdc04480b2602be55c77b97ad817834ff82
Mariano Majano Amaya <mariano@bu.edu>
2024-05-13 16:27:12 -0400

    Starting work on the CartViewModel

---
+++ app/src/main/java/com/example/solemate/viewmodel/CartViewModel.java
@@ -0,0 +1,15 @@
+package com.example.solemate.viewmodel;
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.solemate.repository.CartRepo;
+import com.example.solemate.utils.model.ShoeCart;
+
+import java.util.List;
+
+public class CartViewModel extends AndroidViewModel {
+
+}
